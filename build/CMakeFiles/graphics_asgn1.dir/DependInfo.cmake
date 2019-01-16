4d33357a565a2882f1df655aeeda8849fc00050e
Mayank Musaddi <mayank.musaddi@research.iiit.ac.in>
2019-01-17 00:32:55 +0530

    Basics completed

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/ball.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/color.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/input.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/main.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/nonedit.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/other_handlers.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/platform.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/platform.cpp.o"
+  "/home/mayank/Study/Graphics/jetpack_opengl/src/timer.cpp" "/home/mayank/Study/Graphics/jetpack_opengl/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.cpp
@@ -0,0 +1,180 @@
+#include "main.h"
+#include "timer.h"
+#include "ball.h"
+#include "platform.h"
+
+using namespace std;
+
+GLMatrices Matrices;
+GLuint     programID;
+GLFWwindow *window;
+
+/**************************
+* Customizable functions *
+**************************/
+
+Platform platform;
+Ball ball;
+
+float screen_zoom = 1, screen_center_x = 0, screen_center_y = 0;
+float camera_rotation_angle = 0;
+float ground = 0.1f;
+float gravity = 0.1f;
+
+Timer t60(1.0 / 60);
+
+/* Render the scene with openGL */
+/* Edit this function according to your assignment */
+void draw() {
+    // clear the color and depth in the frame buffer
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // use the loaded shader program
+    // Don't change unless you know what you are doing
+    glUseProgram (programID);
+
+    // Eye - Location of camera. Don't change unless you are sure!!
+    // glm::vec3 eye ( 5*cos(camera_rotation_angle*M_PI/180.0f), 0, 5*sin(camera_rotation_angle*M_PI/180.0f) );
+    glm::vec3 eye ( 0, 0, 1 );
+    // Target - Where is the camera looking at.  Don't change unless you are sure!!
+    glm::vec3 target (0, 0, 0);
+    // Up - Up vector defines tilt of camera.  Don't change unless you are sure!!
+    glm::vec3 up (0, 1, 0);
+
+    // Compute Camera matrix (view)
+    Matrices.view = glm::lookAt( eye, target, up ); // Rotating Camera for 3D
+    // Don't change unless you are sure!!
+    // Matrices.view = glm::lookAt(glm::vec3(0, 0, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)); // Fixed camera for 2D (ortho) in XY plane
+
+    // Compute ViewProject matrix as view/camera might not be changed for this frame (basic scenario)
+    // Don't change unless you are sure!!
+    glm::mat4 VP = Matrices.projection * Matrices.view;
+
+    // Send our transformation to the currently bound shader, in the "MVP" uniform
+    // For each model you render, since the MVP will be different (at least the M part)
+    // Don't change unless you are sure!!
+    glm::mat4 MVP;  // MVP = Projection * View * Model
+
+    // Scene render
+    platform.draw(VP);
+    ball.draw(VP);
+}
+
+void tick_input(GLFWwindow *window) {
+    int left  = glfwGetKey(window, GLFW_KEY_LEFT);
+    int right = glfwGetKey(window, GLFW_KEY_RIGHT);
+    int space = glfwGetKey(window, GLFW_KEY_SPACE);
+    if (left) {
+        ball.move_left();
+        // Do something
+    }
+    else if (right) {
+        ball.move_right();
+    }
+    else if (space) {
+        ball.move_up();
+    }
+}
+
+void tick_elements() {
+    cout << "Y: " << ball.position.y << endl;
+    if(ball.position.y > ground)
+    {
+        ball.position.y-=0.001;
+    }
+    // ball1.tick();
+    // b1.x = ball1.position.x;
+    // b1.y = ball1.position.y;
+    // b2.x = ball2.position.x;
+    // b2.y = ball2.position.y;
+    // b1.width = 2.0f;
+    // b2.width = 2.0f;
+    // b1.height = 2.0f;
+    // b2.height = 2.0f;
+    // if(detect_collision(b1,b2))
+    // {
+    //     ball1.position.y -=ball1.speed;
+    //     ball2.position.y -=ball2.speed;
+    // }
+    // else
+    // {
+    //     ball1.position.x +=ball1.speed;   
+    //     ball2.position.x -=ball2.speed;
+    // }
+    // camera_rotation_angle += 1;
+}
+
+/* Initialize the OpenGL rendering properties */
+/* Add all the models to be created here */
+void initGL(GLFWwindow *window, int width, int height) {
+    /* Objects should be created before any other gl function and shaders */
+    // Create the models
+
+    platform    = Platform(0,0, COLOR_BLACK);
+    ball = Ball(0,ground,COLOR_GREEN);
+
+    // Create and compile our GLSL program from the shaders
+    programID = LoadShaders("Sample_GL.vert", "Sample_GL.frag");
+    // Get a handle for our "MVP" uniform
+    Matrices.MatrixID = glGetUniformLocation(programID, "MVP");
+
+
+    reshapeWindow (window, width, height);
+
+    // Background color of the scene
+    glClearColor (COLOR_BACKGROUND.r / 256.0, COLOR_BACKGROUND.g / 256.0, COLOR_BACKGROUND.b / 256.0, 0.0f); // R, G, B, A
+    glClearDepth (1.0f);
+
+    glEnable (GL_DEPTH_TEST);
+    glDepthFunc (GL_LEQUAL);
+
+    cout << "VENDOR: " << glGetString(GL_VENDOR) << endl;
+    cout << "RENDERER: " << glGetString(GL_RENDERER) << endl;
+    cout << "VERSION: " << glGetString(GL_VERSION) << endl;
+    cout << "GLSL: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+}
+
+
+int main(int argc, char **argv) {
+    srand(time(0));
+    int width  = 600;
+    int height = 600;
+
+    window = initGLFW(width, height);
+
+    initGL (window, width, height);
+
+    /* Draw in loop */
+    while (!glfwWindowShouldClose(window)) {
+        // Process timers
+
+        if (t60.processTick()) {
+            // 60 fps
+            // OpenGL Draw commands
+            draw();
+            // Swap Frame Buffer in double buffering
+            glfwSwapBuffers(window);
+
+            tick_elements();
+            tick_input(window);
+        }
+
+        // Poll for Keyboard and mouse events
+        glfwPollEvents();
+    }
+
+    quit(window);
+}
+
+bool detect_collision(bounding_box_t a, bounding_box_t b) {
+    return (abs(a.x - b.x) * 2 < (a.width + b.width)) &&
+           (abs(a.y - b.y) * 2 < (a.height + b.height));
+}
+
+void reset_screen() {
+    float top    = screen_center_y + 1 / screen_zoom;
+    float bottom = screen_center_y - 0 / screen_zoom;
+    float left   = screen_center_x - 0 / screen_zoom;
+    float right  = screen_center_x + 1 / screen_zoom;
+    Matrices.projection = glm::ortho(left, right, bottom, top, 0.1f, 500.0f);
+}
